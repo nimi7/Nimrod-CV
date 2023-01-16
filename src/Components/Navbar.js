1bddd2e4baa510e2d0317be464fbdc212b133e30
nimi7 <62261707+nimi7@users.noreply.github.com>
2023-01-16 20:08:41 +0200

    Software Developer

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -34,7 +34,7 @@ export default function Navbar() {
                             <div class="sm:text-center lg:text-left">
                                 <h1 class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-5xl">
                                     <span class="block xl:inline">Hi,  <br /> I`m Neriya Wandam</span><br/>
-                                    <span class="block text-indigo-600 xl:inline"> a Front-End developer</span>
+                                    <span class="block text-indigo-600 xl:inline"> a Software Developer</span>
                                 </h1>
                                 <p class="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
                                     I enjoy working on branding, product design, and strategy with passionate people. , whether it's a website or an interface.
